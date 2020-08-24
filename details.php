b3fab6a08aa51912826c8ae69ff93a0ca7269a24
ukkin <nikharshah2000@gmail.com>
2020-08-24 19:07:41 +0530

    corrected the details and home file

--- details.php
+++ details.php
@@ -1,100 +1,94 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="signup.css">
-</head>
-<body>
-  <header>
-<div class="main">
-  <div class="logo">
-    <img src="logo2.png">
-  </div>
-  <ul><b>
-  <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-  </b></ul>
-
-
-<fieldset style = "width: 600px; margin:  400px ">
-<form method="POST" action="#">
-    <div class="new">
-        <div class="white">
-    <div class="left1"><br>
-   
-  </div>
-
-<form action="" method="post">
-  <div class="container">
-    <div class="left2"><h2>Fill the following information</h2><br></div>
-   <div class="left2"> <p></p><br></div>
-    
-
-    <label for="email"><b>Name</b></label><br>
-    <input type="text" placeholder="Enter Name" name="name" required>
-<br><br>
-   <label for="email"><b>Contact Number</b></label><br>
-    <input type="text" placeholder="Enter Contact number" name="contact" required>
-    
-<br><br><label for="email"><b>Email ID</b></label><br>
-    <input type="text" placeholder="Enter Email ID" name="email" required>
-<br><br>
-<form>
-  Select your Category<br>
-  <input type="radio" name="c" value="general" checked> General<br>
-  <input type="radio" name="c" value="obc"> OBC<br>
-  <input type="radio" name="c" value="ST"> ST  <br>
-  <input type="radio" name="c" value="SC"> SC<br>
-</form> <br>
-
-<b>Date of Birth : </b><input type="date" id="date" name="from"> 
-    <div class="clearfix"><br>
-      
-      <button type="submit" name="Upload" class="signupbtn">Submit</button>
-    </div>
-  </div>
-</form>
-
-<?php
-session_start();
-$con=mysqli_connect("localhost","root","","login");
-if(mysqli_connect_errno($con))
-{
-    echo mysqli_connect_error();
-}
-
-if(isset($_POST['Upload']))
-{
-  $reg=$_SESSION['x'];
-  $contact= $_POST['contact'];
-  $name=$_POST['name'];
-  $email=$_POST['email'];
-  $categ=$_POST['c'];
-  $dob=$_POST['from'];
-
-  $sqlDelete="Delete from student_menu where student_id=$_SESSION[x]";
-  $qsql=mysqli_query($con,$sqlDelete);
-$query="INSERT INTO details (student_id, Name, Contact, Email, Category, DOB) VALUES ('$reg', '$name','$contact','$email','$categ','$dob')";
-
-if(mysqli_query($con, $query))
-{
-  header('location: http://localhost/home.php', true, 307);       
-          exit();
-}
-else
-{
-  echo "Error in submitting form";
-}
-
-}
-
-
-?>
-
-
-</body>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="signup.css">
+	</head>
+	<body>
+		<header>
+		<div class="main">
+		<div class="logo">
+			<img src="logo2.png">
+		</div>
+		<ul>
+			<b>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</b>
+		</ul>
+		<fieldset style = "width: 600px; margin:  400px ">
+		<div class="new">
+		<div class="white">
+		<div class="left1"><br>
+		</div>
+		<form action="" method="post">
+			<div class="container">
+				<div class="left2">
+					<h2>Fill the following information</h2>
+					<br>
+				</div>
+				<div class="left2">
+					<p></p>
+					<br>
+				</div>
+				<label for="email"><b>Name</b></label><br>
+				<input type="text" placeholder="Enter Name" name="name" required>
+				<br><br>
+				<label for="email"><b>Contact Number</b></label><br>
+				<input type="text" placeholder="Enter Contact number" name="contact" required>
+				<br><br><label for="email"><b>Email ID</b></label><br>
+				<input type="email" placeholder="Enter Email ID" name="email" required>
+				<br><br>
+				Select your Category<br>
+				<input type="radio" name="c" value="general" checked> General<br>
+				<input type="radio" name="c" value="obc"> OBC<br>
+				<input type="radio" name="c" value="ST"> ST  <br>
+				<input type="radio" name="c" value="SC"> SC<br>
+				<br>
+				<b>Date of Birth : </b><input type="date" id="date" name="dob"> 
+				<div class="clearfix"><br>
+					<button type="submit" name="Upload" class="signupbtn">Submit</button>
+				</div>
+			</div>
+		</form>
+		<?php
+			session_start();
+			$con=mysqli_connect("localhost","root","","login");
+			if(mysqli_connect_errno($con))
+			{
+			    echo mysqli_connect_error();
+			}
+			
+			if(isset($_POST['Upload']))
+			{
+			  $reg=$_SESSION['x'];
+			  $contact= $_POST['contact'];
+			  $name=$_POST['name'];
+			  $email=$_POST['email'];
+			  $categ=$_POST['c'];
+			  $dob=$_POST['dob'];
+			
+			  $sqlDelete="Delete from student_menu where student_id=$_SESSION[x]";
+			  $qsql=mysqli_query($con,$sqlDelete);
+			$query="INSERT INTO details (student_id, Name, Contact, Email, Category, DOB) VALUES ('$reg', '$name','$contact','$email','$categ','$dob')";
+			
+			if(mysqli_query($con, $query))
+			{
+			  header('location: http://localhost/login/home.php', true, 307);       
+			          exit();
+			}
+			else
+			{
+			  echo "Error in submitting form";
+			}
+			
+			}
+			
+			
+			?>
+	</body>
 </html>
--- home.php
+++ home.php
@@ -1,94 +1,83 @@
 <html>
-
-<head>
-
-<link rel="stylesheet" type="text/css" href="home.css">
-</head>
-
-<?php
-
-
-session_start();
-$con=mysqli_connect("localhost","root","","login");
-if(mysqli_connect_errno($con))
-{
-    echo mysqli_connect_error();
-}
-$qsql = "SELECT * FROM details  WHERE student_id='$_SESSION[x]'";
-if(!$qsql = mysqli_query($con,$qsql))
-	{
-		echo mysqli_error($con);
-	}
-	else
-	{
-		if(mysqli_num_rows($qsql)==1)
+	<head>
+		<link rel="stylesheet" type="text/css" href="home.css">
+	</head>
+	<?php
+		session_start();
+		$con=mysqli_connect("localhost","root","","login");
+		if(mysqli_connect_errno($con))
 		{
-			$rs = mysqli_fetch_array($qsql);
-      $reg=$_SESSION['x'];
-      $name=$rs[1];
-      $contact=$rs[2];
-      $email=$rs[3];
-      $categ=$rs[4];
-      $dob=$rs[5];
-     // $name=$rs[1];
-    }
-    }
-
-
-error_reporting(0);
-ini_set('display_errors', 0);
-
-?>
-
-
-</head>
-<body>
-<header>
-<div class="main">
-  <div class="logo">
-    <img src="logo2.png">
-  </div>
-  <ul><b>
-  <li  class="active"><a href="home.php">Home</a></li>
-    <li><a href="form.php">NOC</a></li>
-    <li><a href="scholarship.php">Scholarship</a></li>
-    <li><a href="index.php">Mess Menu</a></li>
-    <li><a href="attendance.php">Attendance</a></li>
-    <li><a href="contact.php">Contact Us</a></li>
-  </b></ul>
-  </div>
-  <h2>
-  <div class="white">
-<fieldset style = "width: 600px; margin:  400px ">
-  <div class="new">
-    <div class="left2">
-<br>
- Registration Number : <?php
- echo $reg; ?><br><br>
-
- Name : <?php
- echo $name; ?><br><br>
-
-  Contact Number : <?php
- echo $contact; ?><br><br>
- 
- Email ID : <?php
- echo $email; ?><br><br>
-
-Category : <?php
- echo $categ; ?><br><br>
- 
- Date of Birth : <?php
- echo $dob; ?><br><br>
-</div>
-<center>
-<a href="details.php">Enter your details</a>
-<br>
-<button style="height:60px;width:200px" name="submit" type="submit"><h1><a href="login_page.php">Logout</a></h1><br></button>
-
-
-  </header>
-
-  
-  </body>
-  </html>
+		    echo mysqli_connect_error();
+		}
+		$qsql = "SELECT * FROM details  WHERE student_id='$_SESSION[x]'";
+		
+		if(!$qsql = mysqli_query($con,$qsql))  
+		{
+			echo mysqli_error($con);
+		}
+		else
+		{
+			if(mysqli_num_rows($qsql)==1)
+			{
+				$rs = mysqli_fetch_array($qsql);
+				$reg=$_SESSION['x'];
+				$name=$rs[1];
+				$contact=$rs[2];
+				$email=$rs[3];
+				$categ=$rs[4];
+				$dob=$rs[5];
+			}
+		}
+		
+		
+		//error_reporting(0);
+		//ini_set('display_errors', 0);
+		
+		?>
+	</head>
+	<body>
+		<header>
+			<div class="main">
+				<div class="logo">
+					<img src="logo2.png">
+				</div>
+				<ul>
+					<b>
+						<li  class="active"><a href="home.php">Home</a></li>
+						<li><a href="form.php">NOC</a></li>
+						<li><a href="scholarship.php">Scholarship</a></li>
+						<li><a href="index.php">Mess Menu</a></li>
+						<li><a href="attendance.php">Attendance</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
+					</b>
+				</ul>
+			</div>
+			<h2>
+			<div class="white">
+			<fieldset style = "width: 600px; margin:  400px ">
+			<div class="new">
+			<div class="left2">
+				<br>
+				Registration Number : <?php
+					echo $reg; ?><br><br>
+				Name : <?php
+					echo $name; ?><br><br>
+				Contact Number : <?php
+					echo $contact; ?><br><br>
+				Email ID : <?php
+					echo $email; ?><br><br>
+				Category : <?php
+					echo $categ; ?><br><br>
+				Date of Birth : <?php
+					echo $dob; ?><br><br>
+			</div>
+			<center>
+			<a href="details.php">Enter your details</a>
+			<br>
+			<button style="height:60px;width:200px" name="submit" type="submit">
+				<h1><a href="login_page.php">Logout</a></h1>
+				<br>
+			</button>
+		</header>
+	</body>
+</html>
