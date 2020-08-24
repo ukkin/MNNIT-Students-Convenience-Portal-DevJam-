632db2fe58893807c9f14a5fe1e8bf44397bcf07
ukkin <nikharshah2000@gmail.com>
2020-08-24 22:32:43 +0530

    corrected sql file

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2020 at 06:58 PM
+-- Generation Time: Aug 24, 2020 at 07:01 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
